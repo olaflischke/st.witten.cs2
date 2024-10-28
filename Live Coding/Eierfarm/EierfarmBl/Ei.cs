b57512ae2ea22f396ef40cb570b3007d2e894b8e
Olaf Lischke <olaf.lischke@lischke-edv.de>
2024-10-28 16:42:54 +0100

    Tag 1

---
+++ Live Coding/Eierfarm/EierfarmBl/Ei.cs
@@ -0,0 +1,68 @@
+﻿namespace EierfarmBl
+{
+    public class Ei
+    {
+        // Konstruktor für ein Ei
+        public Ei()
+        {
+            
+            // Ersetzt durch Auto-Property Initializer
+            //this.Legedatum = DateTime.Now;
+
+            // Instannziiert einen Zufallsgenerator
+            Random random    = new Random();
+
+            // _gewicht = 42;
+            this.Gewicht = random.Next(45, 81);
+            this.Farbe = EiFarbe.Gruen;
+        }
+
+        // Öffentliches Feld
+        // Nicht machen!
+        //public double Gewicht2;
+
+
+        // Full-qualified Property (Property mit Backing Field)
+        // erzeugt über "propfull" 
+
+        // Backing Field
+        private double _gewicht;
+
+        // Öffentlicher Teil der Property
+        public double Gewicht
+        {
+            // Accessoren der Gewicht-Property
+            get { return _gewicht; }    // double g = meinEi.Gewicht;
+            set
+            {
+                //if (value > 0) _gewicht = value;
+                if (value > 0)
+                {
+                    _gewicht = value;
+                }
+            }
+        }    // meinEi.Gewicht = 65;
+
+        public EiFarbe Farbe { get; set; }
+
+        public double GewichtInKilo
+        {
+            get
+            {
+                return this.Gewicht / 1000;
+            }
+        }
+
+        // Auto-Property
+        // Property mit automatisch (durch den Compiler) erzeugtem Backing Field
+        public DateTime Legedatum { get; set; } = DateTime.Now; // Auto-Property Initializer (ausgeführt vor(!) Konstruktor)
+    }
+
+    public enum EiFarbe
+    {
+        Gruen,
+        Hell,
+        Dunkel
+    }
+}
+
---
+++ Live Coding/Eierfarm/EierfarmBl/Huhn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EierfarmBl
+{
+    public class Huhn
+    {
+        public string Name { get; set; }
+        public DateTime Schluepfdatum { get; set; }
+        public double Gewicht { get; set; }
+        public List<Ei> Eier { get; set; } = new List<Ei>();
+
+
+        public void Fressen()
+        {
+            if (this.Gewicht < 3000)
+            {
+                // Gewicht = Gewicht + 100;
+                this.Gewicht += 100;
+            }
+        }
+
+        public void EiLegen()
+        {
+            if (this.Gewicht > 1500)
+            {
+                // Typ Variable = Wert;
+                Ei ei = new Ei(); // ruft den Konstruktor der Ei-Klasse auf
+                this.Gewicht -= ei.Gewicht;
+                this.Eier.Add(ei);
+            }
+        }
+    }
+}
